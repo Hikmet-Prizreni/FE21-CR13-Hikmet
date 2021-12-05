37d692c8e3e36891b8d51d6fa8994380364560e6
Hikmet-Prizreni <88586234+Hikmet-Prizreni@users.noreply.github.com>
2021-12-05 04:18:30 +0100

    commid for the first

---
+++ Hikmets Big Event/src/Controller/ActionsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActionsController extends AbstractController
+{
+    #[Route('/actions', name: 'actions')]
+    public function index(): Response
+    {
+
+        return $this->render('todo/index.html.twig');
+
+    }
+
+
+    #[Route("/create", name:"todo_create")]
+
+    public function create(): Response
+
+    {
+
+        return $this->render('todo/create.html.twig');
+
+    }
+
+
+    #[Route("/edit/{id}", name:"todo_edit")]
+
+    public function edit(): Response
+
+    {
+
+        return $this->render('todo/edit.html.twig');
+
+    }
+
+
+    #[Route("/details/{id}", name:"todo_details")]
+
+    public function details($id): Response
+
+    {
+
+        return $this->render('todo/details.html.twig');
+
+    }
+
+}
+    // {
+    //     return $this->render('actions/index.html.twig', [
+    //         'controller_name' => 'ActionsController',
+    //     ]);
+    // }
